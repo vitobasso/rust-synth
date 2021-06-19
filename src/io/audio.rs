21cc61ea1e2bf9e2be5fdd2743573ea01c459660
Victor Basso <vitobasso@gmail.com>
2021-06-19 20:44:18 -0300

    lib update: cpal

--- src/io/audio.rs
+++ src/io/audio.rs
@@ -1,32 +1,31 @@
-use cpal;
-use self::cpal::{
-    UnknownTypeOutputBuffer::{F32, I16, U16},
-    StreamData::Output,
-    OutputBuffer, Device, Format, EventLoop
-};
 use std::sync::mpsc::Receiver;
+use std::thread;
+use std::time::Duration;
+use cpal::{self, Device, SupportedStreamConfig, StreamConfig};
+use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
+use cpal::SampleFormat::{F32, U16, I16};
 use crate::core::{music_theory::Hz, synth::Sample, tools::Millis};
 
 const LATENCY: Millis = 250;
 
 pub struct Out {
     device: Device,
-    format: Format,
+    config: SupportedStreamConfig,
 }
 
 impl Out {
     pub fn initialize() -> Result<Self, String> {
-        match cpal::default_output_device() {
+        match cpal::default_host().default_output_device() {
             Some(device) =>
-                device.default_output_format()
+                device.default_output_config()
                     .map_err(|e| format!("Failed to get default output format. {:?}", e))
-                    .map(|format| Out { device, format }),
+                    .map(|config| Out { device, config }),
             None => Err("Failed to get default output device".to_string()),
         }
     }
 
     pub fn sample_rate(&self) -> Hz {
-        Hz::from(self.format.sample_rate.0)
+        Hz::from(self.config.sample_rate().0)
     }
 
     pub fn buffer_size(&self) -> usize {
@@ -34,55 +33,39 @@ impl Out {
     }
 
     pub fn start(&self, sound_in: Receiver<Sample>) {
-        start(&self.device, &self.format, sound_in)
-    }
-}
-
-fn start(device: &Device, format: &Format, sound_in: Receiver<Sample>) {
-    let channels = format.channels as usize;
-    let event_loop = EventLoop::new();
-    let stream_id = event_loop.build_output_stream(device, format).unwrap();
-    event_loop.play_stream(stream_id);
-
-    event_loop.run(move |_, data| {
-        match data {
-            Output { buffer: F32(buffer) } => feed_buffer(buffer, &sound_in, channels),
-            Output { buffer: I16(buffer) } => feed_buffer(buffer, &sound_in, channels),
-            Output { buffer: U16(buffer) } => feed_buffer(buffer, &sound_in, channels),
-            _ => panic!("Unexpected buffer type."),
-        }
-    });
-}
-
-fn feed_buffer<T: SampleFromF64>(mut buffer: OutputBuffer<'_, T>, sig_in: &Receiver<Sample>, channels: usize) {
-    for buff_chunks in buffer.chunks_mut(channels) {
-        match sig_in.recv() {
-            Ok(sample) =>
-                for out in buff_chunks.iter_mut() {
-                    *out = T::from_f64(sample);
-                },
-            _ => {
-                panic!("Sample channel hang up?");
-            }
+        match self.config.sample_format() {
+            F32 => self.start_for::<f32>(sound_in),
+            I16 => self.start_for::<i16>(sound_in),
+            U16 => self.start_for::<u16>(sound_in),
         }
     }
+
+    fn start_for<T>(&self, sound_in: Receiver<Sample>)
+        where T: cpal::Sample
+    {
+        let channels = self.config.channels() as usize;
+        let stream = self.device.build_output_stream(
+            &StreamConfig::from(self.config.clone()),
+            move |buffer: &mut [T], _| write(buffer, channels, &sound_in),
+            |e| panic!("{:?}", e))
+            .unwrap_or_else(|e| panic!("{:?}", e));
+        stream.play().unwrap_or_else(|e| panic!("{:?}", e));
+        thread::sleep(Duration::new(u64::MAX, 0))
+        //TODO instead return stream without thread ?
+        //TODO so if I don't need a thread maybe I don't need a Receiver anymore ..
+    }
+
 }
 
-trait SampleFromF64: cpal::Sample {
-    fn from_f64(value: f64) -> Self;
-}
-impl SampleFromF64 for f32 {
-    fn from_f64(value: f64) -> Self {
-        value as f32
+fn write<T>(buffer: &mut [T], channels: usize, sound_in: &Receiver<Sample>)
+where T: cpal::Sample
+{
+    for chunk in buffer.chunks_mut(channels) {
+        let sample = sound_in.recv().unwrap_or_else(|e| panic!("{}", e)) as f32;
+        let cpal_sample: T = cpal::Sample::from(&sample);
+        for place in chunk.iter_mut() {
+            *place = cpal_sample;
+        }
     }
 }
-impl SampleFromF64 for i16 {
-    fn from_f64(value: f64) -> i16 {
-        (value * f64::from(f64::MAX)) as i16
-    }
-}
-impl SampleFromF64 for u16 {
-    fn from_f64(value: f64) -> u16 {
-        ((value * 0.5 + 0.5) * f64::from(u16::MAX)) as u16
-    }
-}
+
